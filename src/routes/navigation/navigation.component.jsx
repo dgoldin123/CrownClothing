95c65d69a7f6ea5154d7220626602584a1d8be24
yzidell <yzidell2@hotmail.com>
2022-08-23 17:59:45 -0400

    cart added

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,13 +2,17 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  //console.log(currentUser);
+  const { isCartOpen } = useContext(CartContext);
   
   return (
     <Fragment>
@@ -22,11 +26,13 @@ const Navigation = () => {
           ) : (
             <Link className='nav-link' to='/auth'>SIGN IN</Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
   );
 };
-//<Link className='nav-link' to='/auth'>SIGN IN</Link>
+
 export default Navigation;
