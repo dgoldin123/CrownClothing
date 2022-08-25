9af8d16c1fbcc02ba4d76e41fbf3a31a1a16506d
yzidell <yzidell2@hotmail.com>
2022-08-25 13:13:08 -0400

    checkout done

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,28 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+import Button from '../button/button.component';
 import './product-card.styles.scss';
 
-import Button from '../button/button.component';
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
 
+  return (
+    <div className='product-card-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button buttonType='inverted' onClick={addProductToCart}>Add to cart</Button>
+    </div>
+  );
+};
+
+export default ProductCard;
+
+/*
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
   return (
@@ -15,5 +36,4 @@ const ProductCard = ({ product }) => {
     </div>
   );
 };
-
-export default ProductCard;
+*/
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,7 +3,6 @@ import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
-
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
