30aae52983e573dad7c2357d96d3af2e78f4855d
yzidell <yzidell2@hotmail.com>
2022-08-29 13:11:42 -0400

    netlify start 5

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -18,7 +18,7 @@ const Navigation = () => {
       <NavigationContainer>
         <LogoContainer to='/'><CrwnLogo /></LogoContainer>
           <NavLinks>
-            <NavLink to='/shop'>SHOP</NavLink>
+            <NavLink to='/shop'>SHOPppp</NavLink>
             {currentUser ? (
               <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>
             ) : (
